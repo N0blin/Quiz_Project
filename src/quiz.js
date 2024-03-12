16c5845744873ddfde6a6fd53032302020d6302f
n0blin <noah.eric.geiger@gmail.com>
2024-03-13 12:02:01 +1300

    Added question vars

---
+++ src/quiz.js
@@ -0,0 +1,23 @@
+//Install Node.js
+
+//Install Readline - Later
+
+const question = (
+    {
+        question : "What is 2 + 2",
+        options :["2", "3", "4", "5"]
+        answer :"4"
+    },
+    {
+        question : "Whats is the capatial of New Zealand"
+        options : ["Auckland", "Wellington", "Nelson", "Christchurch"]
+        answer :"Wellington"
+    }
+    //Add more questions here.
+)
+
+//Declare Quiz function
+function quiz(){
+
+    
+}
