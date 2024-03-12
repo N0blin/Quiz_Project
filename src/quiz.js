c1b48ee5904ef5102662565ce1bf433b6321a8ef
n0blin <noah.eric.geiger@gmail.com>
2024-03-13 12:32:28 +1300

    added quiz function

--- src/quiz.js
+++ src/quiz.js
@@ -2,22 +2,26 @@
 
 //Install Readline - Later
 
-const question = (
-    {
+const questions = (
+    {//Index 0
         question : "What is 2 + 2",
-        options :["2", "3", "4", "5"]
+        options :["2", "3", "4", "5"],
         answer :"4"
     },
-    {
-        question : "Whats is the capatial of New Zealand"
-        options : ["Auckland", "Wellington", "Nelson", "Christchurch"]
+    {//Index 1
+        question : "Whats is the capatial of New Zealand",
+        options : ["Auckland", "Wellington", "Nelson", "Christchurch"],
         answer :"Wellington"
     }
     //Add more questions here.
-)
+);
 
 //Declare Quiz function
 function quiz(){
+    let score = 0;
 
-    
-}
+    //Iterate through questions
+    questions.forEach = ((q, index) => {
+        cons.log(`Question ${index + 1}: ${q.question}`)
+    })
+ }
